4c6b0c23e17d4cf0146b48b263fb4d40d5262bcf
Sakshi Deore <sdeore@ucsd.edu>
2024-11-23 11:38:41 -0800

    testcase sample

--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-const sum = require('./sum'); // Only import the `sum` function from the main file.
+const sum = require('./index'); // Only import the `sum` function from the main file.
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
