804a5516c06898921e0f2fa2c19d41758c986f0f
YueSteveYin <yuy045@ucsd.edu>
2024-11-27 11:09:02 -0800

    logic push

---
+++ petBlock/Pet.js
@@ -0,0 +1,43 @@
+let defaultHp = 100;
+class Pet {
+  constructor(name, price) {
+    this._hp = defaultHp;
+    this._bought = false;
+    this._name = name;
+    this._price = price;
+    //this._type = type;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get price() {
+    return this._price;
+  }
+
+  get hp() {
+    return this._hp;
+  }
+
+  get bought() {
+    return this._bought;
+  }
+
+  set bought(value) {
+    this._bought = value;
+  }
+
+  changeHp(value) {
+    this._hp += value;
+    if (this._hp > 100) {
+      this._hp = 100;
+    }
+    if (this._hp <= 0) {
+      this._hp = 100;           // pet hp changes back to 100 since it is not bought
+      this._bought = false;
+    }
+  }
+}
+
+module.exports = Pet
